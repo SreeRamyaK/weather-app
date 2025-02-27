21e4b11bd5b5d2dfc33960cf65ac70333122d570
Kapuluru Ramya Sree <ramyasreekrc@gmail.com>
2025-02-27 19:57:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,51 @@
+const fetchWeatherData = async (city) => {
+    const url = `https://yahoo-weather5.p.rapidapi.com/weather?format=json&u=f&location=${city}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com',
+            'x-rapidapi-key': '199b974457msh9144fe7d2af71aep1ac5cbjsn89cfe9ab2443'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        const { astronomy, condition, wind, atmosphere } = data.current_observation;
+        
+        document.getElementById("temp").innerHTML = condition.temperature;
+        document.getElementById("min_temp").innerHTML = data.forecasts[0].low;
+        document.getElementById("max_temp").innerHTML = data.forecasts[0].high;
+        document.getElementById("humid").innerHTML = atmosphere.humidity;
+        document.getElementById("sunrise").innerHTML = astronomy.sunrise;
+        document.getElementById("sunset").innerHTML = astronomy.sunset;
+        document.getElementById("wind").innerHTML = wind.speed;
+        document.getElementById("wind_degree").innerHTML = wind.direction;
+        document.getElementById("feels_like").innerHTML = condition.temperature;
 
-// let apikey="4qQ8EVONuHcuew0Xr0F5AQ==2bRUCF5w6sFjAfcs";
-
-// let url="https://api.api-ninjas.com/v1/weather?city=";
-
-const search=document.getElementById("query");
-const APILINK='https://api.api-ninjas.com/v1/weather?x-api-key=4qQ8EVONuHcuew0Xr0F5AQ==2bRUCF5w6sFjAfcs&city=';
-
-
-try{
-// returnWeather(APILINK);
-
-
-
-
-function returnWeather(url){
-    fetch(url).then(res=> res.json()).then(function(data){
-        const temp=data.temp;
-        const wind_speed=data.wind_speed;
-        const humdity=data.humidity;
-        const min_temp=data.min_temp;
-        const max_temp=data.max_temp;
-        const sunrise=data.sunrise;
-        const sunset=data.sunset;
-        const wind_degree= data.wind_degrees;
-        const feels_like=data.feels_like;
-        if(temp>13){
-            var imgElement = document.getElementById('weather_image');
+        let imgElement = document.getElementById('weather_image');
+        if (condition.temperature > 13) {
             imgElement.src = '/images/sunny.jpeg'; 
-        }
-        else if(temp<=10 && wind_speed>4.5){
-            var imgElement = document.getElementById('weather_image');
+        } else if (condition.temperature <= 10 && wind.speed > 4.5) {
             imgElement.src = '/images/windy.jpeg'; 
-        }
-        else if(humdity>80){
-            var imgElement = document.getElementById('weather_image');
+        } else if (atmosphere.humidity > 80) {
             imgElement.src = '/images/rainy.jpeg'; 
         }
-        document.getElementById("temp").innerHTML = temp;
-        document.getElementById("min_temp").innerHTML = min_temp;
-        document.getElementById("max_temp").innerHTML = max_temp;
-        document.getElementById("humid").innerHTML = humdity;
-        document.getElementById("sunrise").innerHTML = sunrise;
-        document.getElementById("sunset").innerHTML = sunset;
-        document.getElementById("wind").innerHTML = wind_speed;
-        document.getElementById("wind_degree").innerHTML = wind_degree;
-        document.getElementById("feels_like").innerHTML = feels_like;
-        
-        
-    })
-}
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const searchItem=search.value;
-    document.getElementById("city_name").innerHTML = searchItem.toUpperCase();
-    const SEARCHAPI = 'https://api.api-ninjas.com/v1/weather?x-api-key=4qQ8EVONuHcuew0Xr0F5AQ==2bRUCF5w6sFjAfcs&city=';
-    if(searchItem){
-        returnWeather(SEARCHAPI+searchItem);
-    }
 
-})
-}
- catch(err){
-    console.log(err.message);
-}
+        console.log('Weather Data:', data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+};
+
+document.getElementById("searchForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const searchItem = document.getElementById("search").value;
+    document.getElementById("city_name").innerHTML = searchItem.toUpperCase();
+    if (searchItem) {
+        fetchWeatherData(searchItem);
+    }
+});
